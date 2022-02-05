03ce393017e84ebf33b43515009b8be83de09833
SmolPrzybs <maciek970@gmail.com>
2022-02-05 23:51:10 +0100

    Dodaj pliki projektów.

---
+++ Lab6z3/Lab6z3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    int tab[9];
+    float srednia = 0, suma = 0;
+
+    srand(time(NULL));
+    for (int i = 0; i < 9; i++)
+    {
+        *(tab + i) = rand() % 10;
+        cout << *(tab + i) << " ";
+        suma += (tab + i);
+    }
+
+    srednia = suma / 9;
+    cout << srednia << endl;
+
+    for (int i = 0; i < 9; i++)
+    {
+        if ((tab + i) > srednia) cout << *(tab + i) << " ";
+    }
+}
